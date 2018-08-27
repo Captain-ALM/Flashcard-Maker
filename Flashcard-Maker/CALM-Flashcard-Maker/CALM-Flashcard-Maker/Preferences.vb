a29fa5e75d597e262d5b5adb2857d40fdc5e0ad5
Captain ALM <32275498+Captain-ALM@users.noreply.github.com>
2018-08-27 11:22:26 +0100

    0.0-Preferences Object
    Preferences Object Done.
    Preferences
    Preferences$ser
    IPreference

---
+++ Flashcard-Maker/CALM-Flashcard-Maker/CALM-Flashcard-Maker/Preferences.vb
@@ -0,0 +1,121 @@
+﻿Imports System.Runtime.Serialization.Formatters.Binary
+Imports System.IO
+
+Public Class Preferences
+    Implements IPreference(Of List(Of IPreference(Of Object)))
+
+    Private nom As String = ""
+    Private prefs As New List(Of IPreference(Of Object))
+
+    Sub New()
+        nom = ""
+        prefs = New List(Of IPreference(Of Object))
+    End Sub
+
+    Public Sub New(name As String)
+        nom = name
+        prefs = New List(Of IPreference(Of Object))
+    End Sub
+
+    Public Function getName() As String Implements IPreference(Of List(Of IPreference(Of Object))).getName
+        Return nom
+    End Function
+
+    Public Sub loadPreference(data As String) Implements IPreference(Of List(Of IPreference(Of Object))).loadPreference
+        Dim lst As List(Of String) = ser.deserialize(data)
+        Dim nr As New Dictionary(Of String, Integer)
+        For Each pref As String In lst
+            Dim dat As String() = pref.Split(":")
+            If dat.Length = 2 Then
+                Dim pname As String = ser.deserialize(dat(0))
+                Dim pdat As String = ser.deserialize(dat(1))
+                Dim i As Integer = -1
+                If nr.ContainsKey(pname) Then
+                    i = getPreferenceIndex(pname, nr(pname))
+                Else
+                    i = getPreferenceIndex(pname)
+                End If
+                If i > -1 Then
+                    prefs(i).loadPreference(pdat)
+                End If
+                If nr.ContainsKey(pname) Then
+                    nr(pname) += 1
+                Else
+                    nr.Add(pname, 1)
+                End If
+            End If
+        Next
+    End Sub
+
+    Private Function getPreferenceIndex(name As String, Optional index As Integer = 0) As Integer
+        Dim cnt As Integer = 0
+        Dim indx As Integer = 0
+        For Each pref As IPreference(Of Object) In prefs
+            If pref.getName() = name Then
+                If cnt = index Then Return indx Else cnt += 1
+            End If
+            indx += 1
+        Next
+        Return -1
+    End Function
+
+    Public Function savePreference() As String Implements IPreference(Of List(Of IPreference(Of Object))).savePreference
+        Dim lst As New List(Of String)
+        For Each pref As IPreference(Of Object) In prefs
+            lst.Add(ser.serialize(pref.getName()) & ":" & ser.serialize(pref.savePreference()))
+        Next
+        Return ser.serialize(lst)
+    End Function
+
+    Public Sub setName(name As String) Implements IPreference(Of List(Of IPreference(Of Object))).setName
+        nom = name
+    End Sub
+
+    Public Function getPreference() As List(Of IPreference(Of Object)) Implements IPreference(Of List(Of IPreference(Of Object))).getPreference
+        Throw New InvalidOperationException("Get preference object using getPreference<IPreference<Object> t>(String name, [Optional] Integer index = 0)")
+    End Function
+
+    Public Function getPreference(Of t As IPreference(Of Object))(name As String, Optional index As Integer = 0) As t
+        Dim indx As Integer = getPreferenceIndex(name, index)
+        If indx < 0 Then Return Nothing
+        Return prefs(indx)
+    End Function
+
+    Public Sub setPreference(pref As List(Of IPreference(Of Object))) Implements IPreference(Of List(Of IPreference(Of Object))).setPreference
+        Throw New InvalidOperationException("Set preference object using setPreference<IPreference<Object> t>(t pref, [Optional] Integer index = 0)")
+    End Sub
+
+    Public Sub setPreference(Of t As IPreference(Of Object))(pref As t, Optional index As Integer = 0)
+        Dim indx As Integer = getPreferenceIndex(pref.getName(), index)
+        If indx < 0 Then Return
+        prefs(indx) = pref
+    End Sub
+
+    Private Class ser
+        Private Shared formatter As New BinaryFormatter()
+        Public Shared Function serialize(obj As Object) As String
+            Dim ms As New MemoryStream()
+            formatter.Serialize(ms, obj)
+            Dim toreturn As String = Convert.ToBase64String(ms.ToArray)
+            ms.Dispose()
+            ms = Nothing
+            Return toreturn
+        End Function
+        Public Shared Function deserialize(ser As String) As Object
+            Dim ms As New MemoryStream(Convert.FromBase64String(ser))
+            Dim toreturn As Object = formatter.Deserialize(ms)
+            ms.Dispose()
+            ms = Nothing
+            Return toreturn
+        End Function
+    End Class
+End Class
+
+Public Interface IPreference(Of t)
+    Sub setPreference(pref As t)
+    Function getPreference() As t
+    Function savePreference() As String
+    Sub loadPreference(data As String)
+    Function getName() As String
+    Sub setName(name As String)
+End Interface
