24e59e055f02654a004064d074eda2774e8448b7
Captain ALM <32275498+Captain-ALM@users.noreply.github.com>
2018-08-30 12:15:48 +0100

    0.0-Lib Assembly Info
    Update reg-library assembly info.

--- Flashcard-Maker/CALM-Flashcard-Maker/CALM-REGEDIT-BASE/My Project/AssemblyInfo.vb
+++ Flashcard-Maker/CALM-Flashcard-Maker/CALM-REGEDIT-BASE/My Project/AssemblyInfo.vb
@@ -8,11 +8,11 @@ Imports System.Runtime.InteropServices
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("CALM-REGEDIT-BASE")> 
-<Assembly: AssemblyDescription("")> 
-<Assembly: AssemblyCompany("")> 
-<Assembly: AssemblyProduct("CALM-REGEDIT-BASE")> 
-<Assembly: AssemblyCopyright("Copyright ©  2018")> 
+<Assembly: AssemblyTitle("CALM REGEDIT Library - Base")> 
+<Assembly: AssemblyDescription("CALM REGEDIT Library - Base")> 
+<Assembly: AssemblyCompany("Captain ALM")> 
+<Assembly: AssemblyProduct("CALM REGEDIT Library - Base")> 
+<Assembly: AssemblyCopyright("Copyright © Captain ALM 2018")> 
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("0.0.0.0")> 
+<Assembly: AssemblyFileVersion("0.0.0.0")> 
--- Flashcard-Maker/CALM-Flashcard-Maker/CALM-REGEDIT/My Project/AssemblyInfo.vb
+++ Flashcard-Maker/CALM-Flashcard-Maker/CALM-REGEDIT/My Project/AssemblyInfo.vb
@@ -8,11 +8,11 @@ Imports System.Runtime.InteropServices
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("CALM-REGEDIT")> 
-<Assembly: AssemblyDescription("")> 
-<Assembly: AssemblyCompany("")> 
-<Assembly: AssemblyProduct("CALM-REGEDIT")> 
-<Assembly: AssemblyCopyright("Copyright ©  2018")> 
+<Assembly: AssemblyTitle("CALM REGEDIT Library - Object")> 
+<Assembly: AssemblyDescription("CALM REGEDIT Library - Object")> 
+<Assembly: AssemblyCompany("Captain ALM")> 
+<Assembly: AssemblyProduct("CALM REGEDIT Library - Object")> 
+<Assembly: AssemblyCopyright("Copyright © Captain ALM 2018")> 
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("0.0.0.0")> 
+<Assembly: AssemblyFileVersion("0.0.0.0")> 
